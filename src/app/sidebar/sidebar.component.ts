867556826e10be44a5ce4e8d62916dbd243d8325
Folifo <niflorent4@gmail.com>
2022-01-13 13:43:42 +0100

    chat Section

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -9,6 +9,18 @@ export class SidebarComponent implements OnInit {
 
   constructor() { }
 
+  conversations=[
+    {name:"James" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false},
+    {name:"David" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : true},
+    {name:"Kelly" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false},
+    {name:"Robert" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : true},
+    {name:"Thomson" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : true},
+    {name:"Garry" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false},
+    {name:"Donald" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false},
+    {name:"Edinson" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false},
+    {name:"Daniel" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false},
+    {name:"Bruce" , time:"8:34 AM", latestMessage: "Good morning !!!", latestMessageRead : false}
+  ];
   ngOnInit(): void {
   }
 
